72cb506012e0d1903a805e2f0d7b26e7f11ef11c
Alex Brouwer <alex@factorit.nl>
2022-08-25 11:02:18 +0200

    Introduced workspace:init command, dropped unimplemented workspace:merge

---
+++ src/Command/Initialize.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Factorit\ComposerWorkspacePlugin\Command;
+
+use Composer\Command\BaseCommand;
+use Composer\Config\JsonConfigSource;
+use Composer\Json\JsonFile;
+use Composer\Util\Filesystem;
+use Composer\Util\Platform;
+use Factorit\ComposerWorkspacePlugin\Factory;
+use Factorit\ComposerWorkspacePlugin\WorkspaceConfig;
+use RuntimeException;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+final class Initialize extends BaseCommand
+{
+    protected function configure(): void
+    {
+        $this->setName('workspace:init');
+        $this->setDescription('Initialize workspace in current directory');
+    }
+
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        $cwd = Platform::getCwd(true);
+        if (file_exists($cwd . DIRECTORY_SEPARATOR . Factory::getComposerFile())) {
+            throw new RuntimeException('Workspace is already initialized.');
+        }
+
+        parent::initialize($input, $output);
+    }
+
+    protected function interact(InputInterface $input, OutputInterface $output)
+    {
+        $io = $this->getIO();
+
+        $question = 'Would you like to define the package locations interactively [<comment>yes</comment>]? ';
+        $packages = $input->getOption('packages');
+        $packagePaths = [];
+        if (count($packages) > 0 || $io->askConfirmation($question)) {
+            $packagePaths = $this->determinePackagePaths($packages);
+        }
+        $input->setOption('packages', $packagePaths);
+    }
+
+    private function determinePackagePaths(array $packages): array
+    {
+        if (count($packages) > 0) {
+            return $this->normalizePackagePaths($packages);
+        }
+
+        $io = $this->getIO();
+        while (null !== $package = $io->ask('Add path to packages: ')) {
+            $packages[] = $package;
+        }
+
+        return $packages;
+    }
+
+    private function normalizePackagePaths(array $packagePaths): array
+    {
+        $fs = new Filesystem();
+
+        return array_filter(
+            array_map(
+                static function ($packagePath) use ($fs): string {
+                    return Platform::expandPath($fs->normalizePath($packagePath));
+                },
+                $packagePaths
+            )
+        );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = $this->getIO();
+
+        $composerFile = new JsonFile(Factory::getComposerFile());
+        $configSource = new JsonConfigSource($composerFile);
+
+        $workspaceRootConfig = WorkspaceConfig::fromJsonFile($composerFile);
+        $workspaceRootConfig->setPackagePaths($input->getOption('packages'));
+
+        $configSource->addProperty('extra.workspace', $workspaceRootConfig->toArray());
+
+        $io->write('<info>Workspace configuration added to composer.json</info>');
+
+        return self::SUCCESS;
+    }
+}
--- src/CommandProvider.php
+++ src/CommandProvider.php
@@ -5,14 +5,14 @@ declare(strict_types=1);
 namespace Factorit\ComposerWorkspacePlugin;
 
 use Composer\Plugin\Capability\CommandProvider as ComposerCommandProvider;
-use Factorit\ComposerWorkspacePlugin\Command\Merge;
+use Factorit\ComposerWorkspacePlugin\Command\Initialize;
 
 final class CommandProvider implements ComposerCommandProvider
 {
     public function getCommands(): array
     {
         return [
-            new Merge()
+            new Initialize(),
         ];
     }
 
